aa31ef6f2ed8f96e9150ce5f6471a82c4afa661e
Elise <elise.maaiza33@gmail.com>
2024-03-11 17:07:22 +0100

    add services

--- mvc/src/main/java/com/example/mvc/Service/EmpruntService.java
+++ mvc/src/main/java/com/example/mvc/Service/EmpruntService.java
@@ -1,5 +1,34 @@
 package com.example.mvc.Service;
 
-public class EmpruntService {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.mvc.Model.Entity.Emprunt;
+import com.example.mvc.Model.Entity.Livre;
+import com.example.mvc.Model.Repository.EmpruntRepository;
+
+@Service
+public class EmpruntService {
+	private EmpruntRepository empruntRepo;
+	
+	@Autowired
+	public EmpruntService(EmpruntRepository empruntRepo) {
+		this.empruntRepo = empruntRepo;
+	}
+	
+	Emprunt saveEmprunt(Emprunt emprunt) {
+		return empruntRepo.save(emprunt);
+	}
+	
+	Iterable<Emprunt> getAllEmprunts(){
+		return empruntRepo.findAll();
+	}
+	
+	void deleteById(Emprunt id) {}
+	
+	Optional<Emprunt> findById(Long id) {
+		return empruntRepo.findById(id);
+	}
 }
--- mvc/src/main/java/com/example/mvc/Service/LivreService.java
+++ mvc/src/main/java/com/example/mvc/Service/LivreService.java
@@ -1,21 +1,34 @@
 package com.example.mvc.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.mvc.Model.Entity.Livre;
-import com.example.mvc.Model.Repository.UtilisateurRepository;
+import com.example.mvc.Model.Repository.LivreRepository;
 
 @Service
 public class LivreService {
-	private LivreService livreService;
+	private LivreRepository livreRepository;
 	
 	@Autowired
-	public LivreService(LivreService livreService) {
-		this.livreService = livreService;
+	public LivreService(LivreRepository livreRepository) {
+		this.livreRepository = livreRepository;
 	}
 	
 	Livre saveLivre(Livre livre) {
-		return livreService.save(livre);
+		return livreRepository.save(livre);
+	}
+
+	Iterable<Livre> getAllLivres(){
+		return livreRepository.findAll();
+	}
+	
+	void deleteById(Livre id) {}
+	
+	Optional<Livre> findById(Long id) {
+		return livreRepository.findById(id);
 	}
 }
